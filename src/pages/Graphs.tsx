3a0c806cd585b8df4bd2cf9a8bb93c58fa0b9fa8
Willem Sleegers <w.sleegers@me.com>
2022-09-04 21:24:55 +0200

    Update Graphs.tsx

--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -9,17 +9,34 @@ const PageGraphs = () => {
   const [histData, setHistData] = useState<Data[]>()
   const [histBins, setHistBins] = useState("25")
   const [histColor, setHistColor] = useState("#24A9D6")
+  const [scatterData, setScatterData] = useState<Data[]>()
   const [faithfulNames, setFaithfulNames] = useState<string[]>()
   const [faithfulName, setFaithfulName] = useState("eruptions")
 
+  // Get the faithful data for the scatter plot and
+  // to extract the names in the data frame
   useEffect(() => {
-    const updateFaithfulNames = async () => {
+    const fetchFaithfulData = async () => {
       const apiURL = process.env.REACT_APP_PUBLIC_URL
-      const response = await get(apiURL + "faithful/names")
-      setFaithfulNames(response.data)
+      const response = await get(apiURL + "faithful/data")
+
+      const data = {
+        x: response.data.map((x: { waiting: number }) => x.waiting),
+        y: response.data.map((x: { eruptions: number }) => x.eruptions),
+      }
+
+      setScatterData([
+        {
+          x: data.x,
+          y: data.y,
+          mode: "markers",
+          type: "scatter",
+        },
+      ])
+      setFaithfulNames(Object.keys(response.data[0]))
     }
 
-    updateFaithfulNames()
+    fetchFaithfulData()
   }, [])
 
   useEffect(() => {
@@ -70,89 +87,118 @@ const PageGraphs = () => {
   return (
     <div className="p-3 w-100">
       <h1>Graphs</h1>
-      <div className="d-flex flex-column flex-lg-row gap-3">
-        <div className="card flex-grow-1" style={{ height: "480px" }}>
-          <div className="card-body overflow-hidden">
-            <Plot
-              className="w-100"
-              data={histData!}
-              layout={{
-                title: "Histogram of " + faithfulName,
-                bargap: 0.01,
-                autosize: false,
-                xaxis: {
-                  title:
-                    faithfulName === "eruptions"
-                      ? "Eruption time in mins"
-                      : "Waiting time to next eruption (in mins)",
-                },
-                yaxis: {
-                  title: "Frequency",
-                },
-              }}
-              config={{ responsive: true }}
-            />
-          </div>
-        </div>
-        <div className="card " style={{ maxWidth: "300px" }}>
-          <div className="card-body">
-            <h4>Controls</h4>
-            <div>
-              <label htmlFor="columnInput" className="form-label">
-                Select column:
-              </label>
-              {faithfulNames && (
-                <select
-                  className="form-select"
-                  id="columnInput"
-                  onChange={onSelectChange}
-                >
-                  {faithfulNames.map((name) => {
-                    return (
-                      <option key={name} value={name}>
-                        {name}
-                      </option>
-                    )
-                  })}
-                </select>
-              )}
-            </div>
-            <div className="mt-3">
-              <label htmlFor="binsRange" className="form-label">
-                Number of bins:
-              </label>
-              <input
-                type="range"
-                id="binsRange"
-                className="form-range"
-                min="1"
-                max="100"
-                onChange={debounce(onSliderChange, 100)}
-              ></input>
-            </div>
-            <div className="mt-2">
-              <label htmlFor="binsText" className="form-label">
-                Number of bins:
-              </label>
-              <input
-                type="number"
-                id="binsText"
-                className="form-control"
-                value={histBins}
-                onChange={onKeyChange}
+
+      <div className="mb-3">
+        <h2>Histogram</h2>
+        <div className="d-flex flex-column flex-lg-row gap-3">
+          <div className="card flex-grow-1" style={{ height: "480px" }}>
+            <div className="card-body overflow-hidden">
+              <Plot
+                className="w-100"
+                data={histData!}
+                layout={{
+                  title: "Histogram of " + faithfulName,
+                  bargap: 0.01,
+                  autosize: false,
+                  xaxis: {
+                    title:
+                      faithfulName === "eruptions"
+                        ? "Eruption time in mins"
+                        : "Waiting time to next eruption (in mins)",
+                  },
+                  yaxis: {
+                    title: "Frequency",
+                  },
+                }}
+                config={{ responsive: true }}
               />
             </div>
-            <div className="mt-3">
-              <label htmlFor="colorInput" className="form-label">
-                Bar color:
-              </label>
-              <input
-                type="color"
-                id="colorInput"
-                className="form-control form-control-color"
-                value={histColor}
-                title="Choose your color"
-                onChange={handleColorChange}
+          </div>
+          <div className="card " style={{ maxWidth: "300px" }}>
+            <div className="card-body">
+              <h4>Controls</h4>
+              <div>
+                <label htmlFor="columnInput" className="form-label">
+                  Select column:
+                </label>
+                {faithfulNames && (
+                  <select
+                    className="form-select"
+                    id="columnInput"
+                    onChange={onSelectChange}
+                  >
+                    {faithfulNames.map((name) => {
+                      return (
+                        <option key={name} value={name}>
+                          {name}
+                        </option>
+                      )
+                    })}
+                  </select>
+                )}
+              </div>
+              <div className="mt-3">
+                <label htmlFor="binsRange" className="form-label">
+                  Number of bins:
+                </label>
+                <input
+                  type="range"
+                  id="binsRange"
+                  className="form-range"
+                  min="1"
+                  max="100"
+                  onChange={debounce(onSliderChange, 100)}
+                ></input>
+              </div>
+              <div className="mt-2">
+                <label htmlFor="binsText" className="form-label">
+                  Number of bins:
+                </label>
+                <input
+                  type="number"
+                  id="binsText"
+                  className="form-control"
+                  value={histBins}
+                  onChange={onKeyChange}
+                />
+              </div>
+              <div className="mt-3">
+                <label htmlFor="colorInput" className="form-label">
+                  Bar color:
+                </label>
+                <input
+                  type="color"
+                  id="colorInput"
+                  className="form-control form-control-color"
+                  value={histColor}
+                  title="Choose your color"
+                  onChange={handleColorChange}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2>Scatter plot</h2>
+        <div className="d-flex flex-column flex-lg-row gap-3">
+          <div className="card flex-grow-1" style={{ height: "480px" }}>
+            <div className="card-body overflow-hidden">
+              <Plot
+                className="w-100"
+                data={scatterData!}
+                layout={{
+                  title: "Scatter plot",
+                  autosize: false,
+                  xaxis: {
+                    title: "Waiting time",
+                  },
+                  yaxis: {
+                    title: "Eruption time",
+                  },
+                }}
+                config={{ responsive: true }}
               />
             </div>
           </div>
