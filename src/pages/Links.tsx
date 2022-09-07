274976b3c2248c0e3aff4c51aa8361480660511f
Willem Sleegers <w.sleegers@me.com>
2022-09-07 23:07:39 +0200

    Added more links

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -3,7 +3,7 @@ const PageLinks = () => {
     <div className="p-3">
       <h1>Links</h1>
       <p>If you liked what you saw, you can find more information here:</p>
-      <ul className="list-group">
+      <ul className="list-group mb-3">
         <li className="list-group-item">
           <a className="text-decoration-none" href="https://www.rplumber.io/">
             plumber
@@ -31,6 +31,34 @@ const PageLinks = () => {
             href="https://github.com/WillemSleegers/r-dashboard"
           >
             My simple React dashboard using the R API
+          </a>
+        </li>
+      </ul>
+
+      <p>Or contact me:</p>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <a
+            className="text-decoration-none"
+            href="https://www.willemsleegers.com"
+          >
+            Via my Quarto website
+          </a>
+        </li>
+        <li className="list-group-item">
+          <a
+            className="text-decoration-none"
+            href="https://twitter.com/willemsleegers"
+          >
+            Via Twitter
+          </a>
+        </li>
+        <li className="list-group-item">
+          <a
+            className="text-decoration-none"
+            href="https://www.linkedin.com/in/willem-sleegers-262a0350/"
+          >
+            Via LinkedIn
           </a>
         </li>
       </ul>
